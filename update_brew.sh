d20a4cd40601f0beedbc67e4e7d80be4918092d9
Steven Tomcavage <stomcavage@weblinc.com>
2015-08-14 09:18:55 -0400

    Add script to install Consolas font, plus miscellaneous changes

---
+++ update_brew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+BREW='/usr/local/bin/brew'
+
+${BREW} update
+${BREW} upgrade
+
--- update_vim_bundles.sh
+++ update_vim_bundles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 GIT=`which git`
 
 START_DIR=`pwd`
