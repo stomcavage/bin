a37da1d7b6f8a45da5b6b58411a2e6a686c1dbd9
Steven Tomcavage <steven@tomcavage.com>
2016-04-04 12:25:28 -0400

    Add monokai

--- install_vim_bundles.sh
+++ install_vim_bundles.sh
@@ -2,11 +2,24 @@
 
 GIT=`which git`
 CABAL=`which cabal`
+CURL=`which curl`
+AUTOVIM=~/.vim/autoload
 VIMDIR=~/.vim/bundle
+VIMCOLOR=~/.vim/colors
 
 [[ $(type -P "$CABAL") ]] || {
   ${CABAL} install hdevtools
 }
+
+mkdir -p ${VIMCOLOR}
+${GIT} clone https://github.com/sickill/vim-monokai.git
+
+ln -s vim-monokai/colors/monokai.vim monokai.vim
+
+mkdir -p  ${AUTOVIM}
+cd ${AUTOVIM}
+
+curl -LSso pathogen.vim https://tpo.pe/pathogen.vim
 
 mkdir -p ${VIMDIR}
 cd ${VIMDIR}
